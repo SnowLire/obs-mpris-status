0b03cec5863af342c3a7a259e0aa9cf2ac44e1d4
SnowLire <snowlire@cumallover.me>
2024-01-05 19:03:53 +0100

    it works!!

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,44 @@
-fn main() {}
+use mpris::{PlaybackStatus, PlayerFinder};
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+type Ps = PlaybackStatus;
+
+fn main() {
+    let player = PlayerFinder::new()
+        .expect("Could not connect to D-Bus")
+        .find_active()
+        .expect("Didn't find any player");
+
+    let mut event = player.events().expect("D-Bus yay");
+
+    let path = Path::new("/home/snowlire/.local/state/music.txt");
+    let display = path.display();
+
+    loop {
+        match event.next() {
+            Some(_) => {}
+            None => continue,
+        };
+
+        let mut file = match File::create(path) {
+            Err(fun) => panic!("couldn't open {display}: {fun}"),
+            Ok(file) => file,
+        };
+
+        match player.get_playback_status().unwrap() {
+            Ps::Paused | Ps::Stopped => {
+                let _ = file.write(b"Stopped/Paused");
+            }
+            Ps::Playing => {
+                let track = player.get_metadata().unwrap();
+                let title = track.title().unwrap();
+                let artist = track.artists().unwrap()[0]; // There's only 1 artist
+
+                let text = format!("Playing: {title} — {artist}");
+                let _ = file.write(text.as_bytes());
+            }
+        }
+    }
+}
